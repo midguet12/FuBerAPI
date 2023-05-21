12b16f39b1d50f718da0ff06cdf198f88cc9a305
Zhircon <agnizahir@gmail.com>
2023-05-21 14:39:07 -0600

    Mensaje de prueba

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 //tarjetaRecarga.crearTarjeta(1,100.00);
-tarjetaRecarga.borrarTarjeta(1);
+tarjetaRecarga.borrarTarjeta(3);
 
 
 const server = http.createServer((req, res) => {
