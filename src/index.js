ae1692caa9cd5af69646cd06f4cf7acbce5a2254
Zhircon <agnizahir@gmail.com>
2023-05-21 19:13:58 -0600

    feat: DAO foto Terminado y probado manualmente

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 const http = require('http');
 
 const tarjetaRecarga = require("./dao/TarjetaRecarga.js");
-
+const foto = require("./dao/Foto.js");
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 //tarjetaRecarga.crearTarjeta(1,100.00);
-tarjetaRecarga.borrarTarjeta(3);
+//tarjetaRecarga.borrarTarjeta(3);
+var url ="https://la_piedrita.jpg";
 
 
+//foto.crearFoto(url);
+//foto.obtenerTodas();
+//foto.buscarPorId(2);
+//foto.borrarFoto(2);
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
