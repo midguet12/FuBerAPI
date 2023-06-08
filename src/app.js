253757e0c88d7d286dede25bf37d36ed9e92da62
Victor <cuevas0122@gmail.com>
2023-06-07 21:18:46 -0600

    Expuse metodos de tarjetaRecarga

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "Express";
 import usuarioRoutes from "./routes/usuario.routes.js";
+import tarjetaRecarga from "./routes/tarjetaRecarga.routes.js";
 
 
 const app = express();
 app.use(express.json());
 app.use(usuarioRoutes);
+app.use(tarjetaRecarga);
 
 export default app;
