849ebeedaaba764f54a5bc226fe97cdaa2d8d9d2
Victor <cuevas0122@gmail.com>
2023-06-07 20:45:23 -0600

    TarjetaRecarga añadida

---
+++ src/routes/tarjetaRecarga.routes.js
@@ -0,0 +1,17 @@
+import {Router} from 'Express';
+import {createTarjeta,getTarjeta,updateTarjeta,updateMonto,deleteTarjeta} from '../controllers/tarjetaRecarga.controller.js'
+const router = Router();
+
+//Crear 
+router.post('/tarjeta', createTarjeta);
+//Obtener individual
+router.get('/tarjeta/:idTarjeta', getTarjeta);
+//Actualizar toda la tarjeta
+router.put('/tarjeta/:idTarjeta', updateTarjeta);
+//Actualizar monto de tarjeta
+router.put('/tarjeta/:idTarjeta', updateMonto);
+//Eliminar
+router.delete('/tarjeta/:idTarjeta', deleteTarjeta)
+
+
+export default router;
