da1c5818dd30350dba7569fcfd48f357d0a02dcf
omi-ttv <omarg2603@hotmail.com>
2023-06-09 18:05:10 -0600

    Pedido Terminaod
    Controller, model y routes de pedido completados

---
+++ src/controllers/pedidos.controller.js
@@ -0,0 +1,67 @@
+import {Pedido} from '../models/Pedido.js';
+
+export const createPedido = async (req, res) =>{
+    try {
+        const {idPedido,idTienda,nombreUsuarioComprador} = req.body;
+        const newPedido= await Producto.create({
+            idPedido,
+            idTienda,
+            nombreUsuarioComprador
+        });
+    res.json(newPedido)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getPedidos = async (req, res) =>{
+    const pedidos = await Pedido.findAll();
+    res.json(pedidos)
+}
+
+export const getPedido = async(req, res) =>{
+    const {idPedido} = req.params;
+
+    try {
+        const pedido = await Pedido.findOne({
+            where: {
+                idPedido,
+            }
+        });
+        res.json(pedido)
+    } catch (error) {
+        console.log(error);
+    }    
+}
+
+export const updatePedido = async(req,res) =>{
+    try {
+        const {idPedido} = req.params;
+        const {idTienda,nombreUsuarioComprador} = req.body;
+
+        const pedido = await Pedido.findByPk(idPedido);
+        pedido.idTienda = idTienda,
+        pedido.nombreUsuarioComprador = nombreUsuarioComprador,
+
+        await pedido.save();
+        res.json(pedido);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const deletePedido = async(req,res) => {
+    try {
+        const {idPedido} = req.params;
+
+        await Pedido.destroy({
+            where: {
+                idPedido,
+            }
+        });
+        return res.sendStatus(204);
+    } catch (error) {
+        console.log(error)
+    }
+}
