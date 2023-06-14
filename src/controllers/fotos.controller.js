21722f8c6caf5dd18c0ebc3d4eb1e251f332abb9
Victor <cuevas0122@gmail.com>
2023-06-14 05:09:11 -0600

    Codigos de error añadidos

--- src/controllers/fotos.controller.js
+++ src/controllers/fotos.controller.js
@@ -3,60 +3,108 @@ import {Foto} from '../models/Foto.js';
 export const createFoto = async (req, res) =>{
     try {
         const {url} = req.body;
-        const newFoto = await Foto.create({
-            url
-        });
-    res.json(newFoto)
+        if(validarURL(url)){
+            const newFoto = await Foto.create({
+                url
+            });
+            res.status(201).json(newFoto)
+        }else{
+            res.status(400).json({message: "Datos invalidos"});
+        }
     } catch (error) {
+        res.json({error:`${error}`})
         console.log(error)
     }
 }
 
 export const getFotos = async (req, res) =>{
-    const fotos = await Foto.findAll();
-    res.json(fotos)
+    try{
+        const fotos = await Foto.findAll();
+        if(fotos!=null){
+            res.status(200).json(fotos)
+        }else{
+            res.status(204).json()
+        }
+    } catch (error){
+        res.json({error:`${error}`})
+        console.log(error)
+    }
 }
 
 export const getFoto = async(req, res) =>{
     const {idFoto} = req.params;
-
-    try {
-        const foto = await Foto.findOne({
-            where: {
-                idFoto,
+    if(idFoto>0){
+        try {
+            const foto = await Foto.findOne({
+                where: {
+                    idFoto,
+                }
+            });
+            if(foto!=null){
+                res.json(foto)
+            }else{
+                res.status(204).json();
             }
-        });
-        res.json(foto)
-    } catch (error) {
-        console.log(error);
-    }    
+        } catch (error) {
+            res.json({error:`${error}`})
+            console.log(error)
+        }    
+    }else{
+        res.status(400).json({message: "Datos invalidos"});
+    }
 }
 
 export const updateFoto = async(req,res) =>{
     try {
         const {idFoto} = req.params;
         const {url} = req.body;
-        const foto = await Foto.findByPk(idFoto);
-        foto.url=url;
-        await foto.save();
-        res.json(foto);
+        if(idFoto>0 && validarURL(url)){
+            const foto = await Foto.findByPk(idFoto);
+            if(foto!=null){
+                foto.url=url;
+                await foto.save();
+                res.status(201).json(foto);
+            }else{
+                res.status(204).json();
+            }
+        }else{
+            res.status(400).json({message: "Datos invalidos"});
+        }
     } catch (error) {
-        console.log(error);
+        res.json({error:`${error}`})
+        console.log(error)
     }
 }
 
 export const deleteFoto = async(req,res) => {
     try {
         const {idFoto} = req.params;
-
-        await Foto.destroy({
-            where: {
-                idFoto,
+        if(idFoto > 0){
+            if(await Foto.findByPk(idFoto) != null){
+                await Foto.destroy({
+                    where: {
+                        idFoto,
+                    }
+                });
+                res.status(200).json();
+            }else{
+                res.status(204).json();
             }
-        });
-
-        return res.sendStatus(204);
+        }else{
+            res.status(400).json({message: "Datos invalidos"});
+        }
     } catch (error) {
+        res.json({error:`${error}`})
         console.log(error)
+    }
+}
+
+ 
+function validarURL(miurl) {
+    try {
+        new URL(miurl);
+        return true;
+    } catch (err) {
+        return false;
     }
 }
--- src/controllers/tarjetaRecarga.controller.js
+++ src/controllers/tarjetaRecarga.controller.js
@@ -1,33 +1,48 @@
 import {TarjetaRecarga} from '../models/TarjetaRecarga.js';
-
+//import {Tienda} from '../models/' IMPORTAR TIENDA
 export const createTarjeta = async (req, res) =>{
     try {
-        const {idTarjeta,idTienda,monto} = req.body;
+        const {idTienda,monto} = req.body;
+        if(idTienda>0 && monto>0){
+            //PONER UNA VALIDACION SI EXISTE LA TIENDA
+            //await Tienda.findByPk(idTienda) != null
+            const newTarjeta = await TarjetaRecarga.create({
+                idTienda,
+                monto
+            });
+            res.status(201).json(newTarjeta)
+        }else{
+            res.status(400).json({message: "Datos invalidos"});
+        }
 
-        const newTarjeta = await TarjetaRecarga.create({
-            idTienda,
-            monto
-        });
-
-    res.json(newTarjeta)
     } catch (error) {
+        res.json({error:`${error}`})
         console.log(error)
     }
 }
 
 export const getTarjeta = async(req, res) =>{
     const {idTarjeta} = req.params;
-
-    try {
-        const tarjeta = await TarjetaRecarga.findOne({
-            where: {
-                idTarjeta,
+    if(idTarjeta>0){
+        try {
+            const tarjeta = await TarjetaRecarga.findOne({
+                where: {
+                    idTarjeta,
+                }
+            });
+            if(tarjeta != null){
+                res.status(200).json(tarjeta)
+            }else{
+                res.status(204).json()
             }
-        });
-        res.json(tarjeta)
-    } catch (error) {
-        console.log(error);
-    }    
+        } catch (error) {
+            res.json({error:`${error}`})
+            console.log(error)
+        }    
+
+    }else{
+        res.status(400).json({message: "Datos invalidos"});
+    }
 }
 
 export const updateTarjeta = async(req,res) =>{
@@ -35,15 +50,23 @@ export const updateTarjeta = async(req,res) =>{
         const {idTarjeta} = req.params;
         const {idTienda, monto} = req.body;
 
-        const tarjeta = await TarjetaRecarga.findByPk(idTarjeta);
-        tarjeta.idTienda = idTienda;
-        tarjeta.monto = monto;
-
-        await tarjeta.save();
-        res.json(tarjeta);
-
+        if(idTarjeta>0 && idTarjeta>0){
+            const tarjeta = await TarjetaRecarga.findByPk(idTarjeta);
+            if(tarjeta!=null){
+                tarjeta.idTienda = idTienda;
+                tarjeta.monto = monto;
+        
+                await tarjeta.save();
+                res.status(201).json(tarjeta);
+            }else{
+                res.status(204).json();
+            }
+        }else{
+            res.status(400).json({message: "Datos invalidos"});
+        }
     } catch (error) {
-        console.log(error);
+        res.json({error:`${error}`})
+        console.log(error)
     }
 }
 
@@ -52,14 +75,23 @@ export const updateMonto = async(req,res) =>{
         const {idTarjeta} = req.params;
         const {monto} = req.body;
 
-        const tarjeta = await TarjetaRecarga.findByPk(idTarjeta);
-        tarjeta.monto = monto;
-
-        await tarjeta.save();
-        res.json(tarjeta);
+        if(idTarjeta>0 && monto>0){
+            const tarjeta = await TarjetaRecarga.findByPk(idTarjeta);
+            if(tarjeta!=null){
+                tarjeta.monto += monto;
+        
+                await tarjeta.save();
+                res.status(201).json(tarjeta);
+            }else{
+                res.status(204).json();
+            }
+        }else{
+            res.status(400).json({message: "Datos invalidos"});
+        }
 
     } catch (error) {
-        console.log(error);
+        res.json({error:`${error}`})
+        console.log(error)
     }
 }
 
@@ -67,14 +99,23 @@ export const deleteTarjeta = async(req,res) => {
     try {
         const {idTarjeta} = req.params;
 
-        await TarjetaRecarga.destroy({
-            where: {
-                idTarjeta,
+        if(idTarjeta > 0){
+            if(await TarjetaRecarga.findByPk(idTarjeta) != null){
+                await TarjetaRecarga.destroy({
+                    where: {
+                        idTarjeta,
+                    }
+                });
+                res.status(200).json();
+            }else{
+                res.status(204).json();
             }
-        });
+        }else{
+            res.status(400).json({message: "Datos invalidos"});
+        }
 
-        return res.sendStatus(204);
     } catch (error) {
+        res.json({error:`${error}`})
         console.log(error)
     }
 }
